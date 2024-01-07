520120dfcb2314e725ba343e07c6b19adf87740a
Noel Mascarenhas <132780449+NoelMascarenhas@users.noreply.github.com>
2024-01-07 18:56:34 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (41.47%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int sumOfNat = n*(n+1)/2;
+        int reqNo = sumOfNat - sum;
+        return reqNo;
+    }
+}
