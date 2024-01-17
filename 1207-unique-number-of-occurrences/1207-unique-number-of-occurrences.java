a1c8327965376e0492ccd8225f6a00c74df553ac
Noel Mascarenhas <132780449+NoelMascarenhas@users.noreply.github.com>
2024-01-17 09:48:31 +0530

    Time: 2 ms (98.10%), Space: 41.7 MB (28.27%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -4,20 +4,7 @@
             for(int i : arr){
                 map.put(i,map.getOrDefault(i,0)+1);
             }
-            int maxCount = 0;
-            for (int count : map.values()) {
-                maxCount = Math.max(maxCount, count);
-            }
-
-            // Check for unique occurrences
-            int[] occurrences = new int[maxCount + 1];
-            for (int count : map.values()) {
-                if (occurrences[count] == 1) {
-                    return false; // Duplicate occurrence found
-                }
-                occurrences[count] = 1;
-            }
-
-            return true;
+            HashSet<Integer> set = new HashSet<>(map.values());
+            return map.size()==set.size();
         }
     }
