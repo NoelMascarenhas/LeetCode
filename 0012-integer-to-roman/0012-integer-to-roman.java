d197e6ed48e73116dfec1e317f5a2bb14acd4685
Noel Mascarenhas <132780449+NoelMascarenhas@users.noreply.github.com>
2024-01-27 19:27:40 +0530

    Time: 3 ms (98.12%), Space: 44.7 MB (26.92%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,7 +1,7 @@
 class Solution {
     public String intToRoman(int num) {
         int[] nosInInt = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<nosInInt.length;i++){
             while(num >= nosInInt[i]){
