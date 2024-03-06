c383492527defebc1c24524125a142ce0623e563
Noel Mascarenhas <132780449+NoelMascarenhas@users.noreply.github.com>
2024-03-06 19:25:02 +0530

    Time: 0 ms (100.00%), Space: 44 MB (93.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,16 +14,15 @@ public class Solution {
         if(head==null){
             return false;
         }
-        ListNode slow =head;
+        ListNode slow=head;
         ListNode fast=head;
-        while(fast !=null && fast.next!=null){
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(fast==slow){
+            if(slow==fast){
                 return true;
             }
         }
         return false;
-        
     }
 }
